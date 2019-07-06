d03d52062e13127fc160075bb2f464f16ac5e4fa
sam <samcodesit@gmail.com>
2019-07-06 21:36:07 +0100

    Add command line arguments for configuring the cpp build

--- Main.hx
+++ Main.hx
@@ -8,7 +8,7 @@ import format.png.Writer;
 import geometrize.Model.ShapeResult;
 import geometrize.Util;
 import geometrize.bitmap.Bitmap;
-import geometrize.bitmap.Rgba;
+import geometrize.exporter.ShapeJsonExporter;
 import geometrize.exporter.SvgExporter;
 import geometrize.runner.ImageRunner;
 import geometrize.runner.ImageRunnerOptions;
@@ -18,25 +18,126 @@ import sys.io.File;
 import sys.io.FileInput;
 import sys.io.FileOutput;
 
+using StringTools;
+
 /**
- * Minimal native (C++ target) demo using Geometrize Haxe to recreate images with geometric primitives.
- * @author Sam Twidale (https://www.geometrize.co.uk/)
+   Minimal native (C++ target) demo using Geometrize Haxe to recreate images with geometric primitives
+   Pass in command line options to transform an image into shapes, for example:
+
+   ./Main.exe -i monarch_butterfly.png -o monarch_butterfly_out.png -t rotated_rectangle -s 500
+
+   @author Sam Twidale (https://www.geometrize.co.uk/)
  */
-class Main
+class Main extends mcli.CommandLine
 {
+	/**
+	   The filepath to load the input image from
+	**/
+	public var i:String = null;
+	/**
+	   The filepath to save the output image, JSON data or SVG
+	**/
+	public var o:String = null;
+	/**
+	   The type of shape to use. One of: rectangle, rotated_rectangle, triangle, ellipse, rotated_ellipse, circle, line, quadratic_bezier, polyline
+	**/
+	public var t:String = "rotated_rectangle";
+	/**
+	   Number of shapes to use in the output image
+	**/
+	public var s:Int = 250;
+	/**
+	   The number of candidate shapes per shape added to the output image
+	**/
+	public var c:Int = 500;
+	/**
+	   The maximum number of times to mutate each candidate shape
+	**/
+	public var m:Int = 100;
+	/**
+	   The opacity (0-255) of each shape added to the output image
+	**/
+	public var a:Int = 128;
+	
+	public function runDefault() {
+		Sys.println("Running Geometrize Haxe...");
+		
+		// Read the input image
+		var sourceBitmap:Bitmap = readPNGImage(new Path(i));
+		if (sourceBitmap == null) {
+			Sys.println("Failed to read PNG image at: " + i);
+			return;
+		}
+		
+		// Populate the image runner options
+		var imageRunnerOptions:ImageRunnerOptions = {
+			shapeTypes: [ stringToShapeType(t) ],
+			alpha: a,
+			candidateShapesPerStep: c,
+			shapeMutationsPerStep: m
+		};
+		
+		// Create image runner and add shapes one by one
+		var runner:ImageRunner = new ImageRunner(sourceBitmap, Util.getAverageImageColor(sourceBitmap));
+		var shapeData:Array<ShapeResult> = [];
+		for (count in 0...s) {
+			var results = runner.step(imageRunnerOptions);
+			for (result in results) {
+				shapeData.push(result);
+				Sys.println(Std.string(count) + ": Adding shape of type " + Std.string(result.shape.getType()));
+			}
+		}
+		
+		// Save the output image/data
+		if (o.endsWith(".png")) {
+			try {
+				writePNGImage(new Path(o), runner.getImageData());
+			} catch (e:Dynamic) {
+				Sys.println("Failed to save PNG image to disk with exception: " + Std.string(e));
+			}
+		} else if (o.endsWith(".svg")) {
+			try {
+				writeSVGImage(new Path(o), SvgExporter.export(shapeData, sourceBitmap.width, sourceBitmap.height));
+			} catch(e:Dynamic) {
+				Sys.println("Failed to save SVG data to disk with exception: " + Std.string(e));
+			}
+		} else if (o.endsWith(".json")) {
+			try {
+				writeJSONData(new Path(o), ShapeJsonExporter.export(shapeData));
+			} catch (e:Dynamic) {
+				Sys.println("Failed to save JSON data to disk with exception: " + Std.string(e));
+			}
+		} else {
+			Sys.println("Failed to identify output path file extension, it should be .png, .svg or .json");
+		}
+	}
+	
+	private function new() {
+		super();
+	}
+	
+	// Prints command-line help string
+	public function help() {
+		Sys.println(this.toString());
+	}
+	
 	// Reads a PNG image from disk and returns an RGBA8888 bitmap
 	private static function readPNGImage(filePath:Path):Bitmap {
-		var handle:FileInput = sys.io.File.read(filePath.toString(), true);
-		var d:Data = new format.png.Reader(handle).read();
-		var hdr:Header = format.png.Tools.getHeader(d);
-		handle.close();
-		
-		// Convert data to RGBA format
-		var bytes = Tools.extract32(d);
-		Tools.reverseBytes(bytes);
-		var rgba = PixelFormatHelpers.argbToRgba(bytes);
-		
-		return Bitmap.createFromBytes(hdr.width, hdr.height, rgba);
+		try {
+			var handle:FileInput = sys.io.File.read(filePath.toString(), true);
+			var d:Data = new format.png.Reader(handle).read();
+			var hdr:Header = format.png.Tools.getHeader(d);
+			handle.close();
+			
+			// Convert data to RGBA format
+			var bytes = Tools.extract32(d);
+			Tools.reverseBytes(bytes);
+			var rgba = PixelFormatHelpers.argbToRgba(bytes);
+			
+			return Bitmap.createFromBytes(hdr.width, hdr.height, rgba);
+		} catch (e:Dynamic) {
+			return null;
+		}
 	}
 	
 	// Writes an RGBA8888 Bitmap to disk as a PNG image
@@ -50,38 +151,38 @@ class Main
 		sys.io.File.saveContent(filePath.toString(), svgData);
 	}
 	
-	// TODO options from ini file or command line?
+	// Writes shape JSON data to disk
+	private static function writeJSONData(filePath:Path, jsonData:String):Void {
+		sys.io.File.saveContent(filePath.toString(), jsonData);
+	}
+	
+	// Try to convert a string to a shape type
+	private static function stringToShapeType(type:String):ShapeType {
+		return switch(type) {
+			case "rectangle":
+				ShapeType.RECTANGLE;
+			case "rotated_rectangle":
+				ShapeType.ROTATED_RECTANGLE;
+			case "triangle":
+				ShapeType.TRIANGLE;
+			case "ellipse":
+				ShapeType.ELLIPSE;
+			case "rotated_ellipse":
+				ShapeType.ROTATED_ELLIPSE;
+			case "circle":
+				ShapeType.CIRCLE;
+			case "line":
+				ShapeType.LINE;
+			case "quadratic_bezier":
+				ShapeType.QUADRATIC_BEZIER;
+			default:
+				Sys.println("Did not recognize provided shape type string, defaulting to circles");
+				ShapeType.CIRCLE;
+		}
+	}
+	
+	// Entry point
 	private static function main() {
-		var imageRunnerOptions:ImageRunnerOptions = {
-			shapeTypes: [ ShapeType.CIRCLE, ShapeType.ELLIPSE, ShapeType.ROTATED_ELLIPSE ],
-			alpha: 128,
-			candidateShapesPerStep: 250,
-			shapeMutationsPerStep: 50
-		};
-		var sourceImagePath:Path = new Path("../sample_images/grapefruit.png");
-		var sourceBitmap:Bitmap = readPNGImage(sourceImagePath);
-		var backgroundColor:Rgba = Util.getAverageImageColor(sourceBitmap);
-		var runner:ImageRunner = new ImageRunner(sourceBitmap, backgroundColor);
-		
-		// TODO
-		var shapeData:Array<ShapeResult> = [];
-		for (i in 0...500) {
-			var results = runner.step(imageRunnerOptions);
-			for (result in results) {
-				shapeData.push(result);
-			}
-		}
-		
-		try {
-			writeSVGImage(new Path("output.svg"), SvgExporter.export(shapeData, sourceBitmap.width, sourceBitmap.height));
-		} catch(e:Dynamic) {
-			// TODO
-		}
-		
-		try {
-			writePNGImage(new Path("output.png"), runner.getImageData());
-		} catch (e:Dynamic) {
-			// TODO
-		}
+		new mcli.Dispatch(Sys.args()).dispatch(new Main());
 	}
 }
--- README.md
+++ README.md
@@ -3,13 +3,45 @@
 [![License](https://img.shields.io/:license-mit-blue.svg?style=flat-square)](https://github.com/Tw1ddle/geometrize-haxe-example/blob/master/LICENSE)
 [![Build Status](https://img.shields.io/travis/Tw1ddle/geometrize-haxe-example.svg?style=flat-square)](https://travis-ci.org/Tw1ddle/geometrize-haxe-example)
 
-WORK IN PROGRESS
-
 Minimal example of the [Geometrize Haxe library](https://github.com/Tw1ddle/geometrize-haxe/), a Haxe library for converting images into shapes.
+
+## Build
+
+Install the dependencies:
+
+```
+haxelib install hxcpp
+haxelib install format
+haxelib install mcli
+haxelib install Sure
+```
+
+Either open the project FlashDevelop and build for Windows, or run Haxe from the command line:
+
+```
+haxe GeometrizeHaxeExample.hxml
+```
 
 ## Usage
 
-This demo should work on native targets. Copy the binary to the [sample images](https://github.com/Tw1ddle/geometrize-haxe-example/tree/master/sample_images) folder and start geometrizing some images. Edit the file "options.txt" in the folder to specify the options the program should use for converting images into shapes.
+This demo works on the C++ target. Move the built binary to the [sample images](https://github.com/Tw1ddle/geometrize-haxe-example/tree/master/sample_images) folder and start geometrizing some images. Pass command line options to specify the options the program should use for converting images into shapes.
+
+```
+# Create an image made of 400 circles
+Main.exe -i sliced_fruit.png -o sliced_fruit_out.png -t circle -s 400
+```
+
+## Options
+
+Flag            | Description    | Default    |
+--------------- | ---------------| ---------|
+i               | The filepath to load the input image from | n/a
+o               | The filepath to save the output image, JSON data or SVG | n/a
+t               | The type of shape to use | One of: rectangle, rotated_rectangle, triangle, ellipse, rotated_ellipse, circle, line, quadratic_bezier, polyline
+s               | Number of shapes to use in the output image | 250
+c               | The number of candidate shapes per shape added to the output image | 500
+m               | The maximum number of times to mutate each candidate shape | 100
+a               | The opacity (0-255) of each shape added to the output image | 128
 
 ## Notes
-* Got an idea or suggestion? Open an issue on GitHub, or send Sam a message on [Twitter](https://twitter.com/Sam_Twidale).
+ * Got an idea or suggestion? Open an issue or send Sam a message on [Twitter](https://twitter.com/Sam_Twidale).
