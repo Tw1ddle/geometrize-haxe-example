47d4dcd6a10e5e57e336f94d1117788f161345a4
sam <samcodesit@gmail.com>
2019-07-06 02:14:15 +0100

    Add submodule

--- Main.hx
+++ Main.hx
@@ -1,5 +1,9 @@
 package;
 
+import geometrize.bitmap.Bitmap;
+import geometrize.runner.ImageRunner;
+import geometrize.bitmap.Rgba;
+
 /**
  * Minimal native-only demo using Geometrize Haxe to recreate images with geometric primitives.
  * @author Sam Twidale (https://www.geometrize.co.uk/)
@@ -8,5 +12,7 @@ class Main
 {
 	private static function main():Void {
 		// TODO need to actually implement this
+		var bitmap = Bitmap.create(0, 0, Rgba.create(255, 128, 255, 255));
+		//var runner = new geometrize.runner.ImageRunner();
 	}
 }
